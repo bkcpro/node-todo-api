7aaa74debc5f6e386aa0983cad96d617e85010b0
bkcpro <you@example.com>
2019-03-17 13:32:48 -0700

    commit test requiring modules

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 //server.js only should have express routes
+var express = require('express');
+var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
