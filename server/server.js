c3f63ff6777b589392c758d26ac1447ef1b62a51
bkcpro <intuitivegrad30@gmail.com>
2019-03-18 23:20:44 -0700

    retrieve individual todos by using Id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectID} = require('mongodb');
 
 
 var app = express();
@@ -19,6 +20,34 @@ app.post('/todos', (req, res) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos', (req, res) => {
+
+  Todo.find().then((todos) => {
+    res.send({
+      todos
+    });
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send(JSON.stringify(todo, undefined, 2));
+  }, () => {
+    res.status(400).send();
   });
 });
 
