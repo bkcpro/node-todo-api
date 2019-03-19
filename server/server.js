3bfb3524ca32429c90856f2db948ec13735f086d
bkcpro <intuitivegrad30@gmail.com>
2019-03-19 01:10:00 -0700

    individual todo test suite

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.get('/todos/:id', (req, res) => {
     if(!todo){
       return res.status(404).send();
     }
-    res.send(JSON.stringify(todo, undefined, 2));
+    res.send({todo});
   }, () => {
     res.status(400).send();
   });
