9282a82e3039ff66ca5d4074cb95d32c462071b3
bkcpro <intuitivegrad30@gmail.com>
2019-03-30 15:13:27 -0700

    heroku changes

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,7 @@ if(env === 'development'){
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 }
-else {
+else if(env === 'test'){
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 }
